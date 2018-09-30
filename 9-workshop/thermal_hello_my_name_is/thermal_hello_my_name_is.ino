b0c73ec977b0e6e398cad9c84e61805f829edfe1
phil <uniphil@gmail.com>
2018-09-30 09:16:53 -0400

    sticker sketch

--- 9-workshop/thermal_hello_my_name_is/thermal_hello_my_name_is.ino
+++ 9-workshop/thermal_hello_my_name_is/thermal_hello_my_name_is.ino
@@ -1,29 +1,50 @@
 #include <SoftwareSerial.h>
 #include <Adafruit_Thermal.h>
+#include <MicroView.h>
 
-SoftwareSerial SSer(5, 6);  // soft RX, TX
-Adafruit_Thermal Thermal(&SSer);
+SoftwareSerial ser(5, 6);  // soft RX, TX
+Adafruit_Thermal Thermal(&ser);
 
 void setup() {
-  pinMode(7, OUTPUT);
-  digitalWrite(7, LOW);  // ground
+  String name = "Phil";
+  String pronouns = "they/him";
 
-  
-  SSer.begin(19200);  // Setup soft serial for ThermalPrinter control
-  while(!SSer);  // fix for leonardo
-  Thermal.begin(0xFF);  // as dark as we can get for label printing
+  uView.begin();
+  uView.clear(PAGE);
+  uView.print("Welcome,\n");
+  uView.print(name);
+  uView.print("!");
+  uView.display();
 
+  pinMode(2, OUTPUT); digitalWrite(2, LOW);
+  pinMode(3, INPUT); digitalWrite(3, HIGH);
+
+  ser.begin(9600);
+  Thermal.begin(0xCC);  // as dark as we can get for label printing
+
+  Thermal.boldOn();
   Thermal.println("Hello world!");
-  Thermal.println("\nmy name is");
+  Thermal.boldOff();
+
+  Thermal.println("\n\nmy name is\n");
+  
   Thermal.doubleHeightOn();
   Thermal.doubleWidthOn();
-  Thermal.println("Eren");
+  Thermal.println(name);
   Thermal.doubleWidthOff();
   Thermal.doubleHeightOff();
-  Thermal.println("\n\nones and zeros: binary solo");
+  
+  Thermal.feed();
+  Thermal.println(pronouns);
+
+//  Thermal.feed(12);
+  Thermal.println("@onesandzeros: binary solo");
+  Thermal.feed(5);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (!digitalRead(3)) {
+    Thermal.feedRows(1);
+    delay(16);
+  }
 }
